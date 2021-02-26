d876629484ef2a4c845b2b8c54e329624b135c67
Lucas Angelo <lucasg258@gmail.com>
2021-02-26 11:08:46 -0300

    Upload de imagens

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,33 +1,43 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "../config/upload";
 
-import CreateUserService from '../services/CreateUserService';
+import CreateUserService from "../services/CreateUserService";
 
-import ensureAunthenticated from '../middlewares/ensureAunthenticated';
+import ensureAunthenticated from "../middlewares/ensureAunthenticated";
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post("/", async (request, response) => {
   try {
-      const {name, email, password} = request.body;
+    const { name, email, password } = request.body;
 
-      const createUser = new CreateUserService();
+    const createUser = new CreateUserService();
 
-      const user = await createUser.execute({ 
-          name,
-          email,
-          password,
-      });
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
 
-      delete user.password;
-    
+    delete user.password;
+
     return response.send(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
-usersRouter.patch('/avatar', ensureAunthenticated, async (request, response) => {
-  return response.json({ok: true});
-})
+usersRouter.patch(
+  '/avatar',
+  ensureAunthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+  console.log(request.file);
+
+    return response.json({ ok: true });
+  }
+);
 
 export default usersRouter;
