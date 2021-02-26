ca8c3e2061eb37395c8cfb560cb1fa2e95e61b3f
Lucas Angelo <lucasg258@gmail.com>
2021-02-26 13:35:05 -0300

    avatar update complete

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,13 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
-    storage: multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'tmp'), 
+    directory: tmpFolder,
+    
+    storage: multer.diskStorage({ 
+        destination: tmpFolder, 
         filename(request, file, callback){
             const fileHash = crypto.randomBytes(10).toString('hex');
             const fileName = `${fileHash}=${file.originalname}`;
